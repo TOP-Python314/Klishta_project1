950be3e6a55efb13bbb6fef5d2c50d944bcbb932
Danil <klishta.danil@yandex.ru>
2024-01-28 20:01:33 +0500

    Прописал функцию для просчёта выигрышных комбинаций, в зависимости от размера игрового поля.

--- WinComb.py
+++ WinComb.py
@@ -1,2 +1,18 @@
-def win_comb(numb:int = 3) -> list:
-    pass
+def win_comb(number:int = 3) -> list | set:
+    a = [num for num in range(number**2)]
+    result = []
+
+    result.append(a[::(number+1)])
+    result.append(a[number-1:number**2 - 1:number-1])
+
+    for i in range(number):
+        b = a[i*number: (number) + number*i]
+        result.append(b)
+
+    for j in range(number):
+        b = a[j::number]
+        result.append(b)
+    
+    result = [set(x) for x in result]
+
+    return result
